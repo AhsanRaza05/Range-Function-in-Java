04259e5be136e954363ecc9f47913c15155c8ce6
Ahsan Raza <ahsanrazamemon2000@gmail.com>
2022-06-04 12:32:46 +0500

    Added the Overloadded function for default step size to 1

--- Range.java
+++ Range.java
@@ -3,12 +3,16 @@ import java.util.*;
 
 class Range{
 
+    static int[] range(int firstValue, int lastValue){
+
+        return(range( firstValue,  lastValue, 1));
+    }
+
 	static int[] range(int firstValue, int lastValue, int stepSize){
 
         int tempFirst = Math.abs(firstValue);
         int tempLast = Math.abs(lastValue);
 
-        //System.out.println(Math.ceil(Math.abs(lastValue - firstValue ) / (double)stepSize));
         int elementQty = (int)Math.ceil(Math.abs(lastValue - firstValue ) / (double)stepSize);
 
         // TEST CASES / Input Possibilities
@@ -16,8 +20,8 @@ class Range{
         // 3-1     =  2 (1,2)         [Increasing Order]
         // 3-(-1)  =  4 (-1,0,1,2)    [Increasing Order]
 
-        // -3-1    = -4 (1,0,-1,-2)   [Decreasing Order] {Invalid case}
-        // -5-(-1) = -4 (-1,-2,-3,-4) [Decreasing Order]
+        // -3-1    = -4 (1,0,-1,-2)   [Decreasing Order] {Invalid case: Not work in Python}
+        // -5-(-1) = -4 (-1,-2,-3,-4) [Decreasing Order] {Invalid case: Not work in Python}
 
         int arr[] = new int[elementQty];
 
@@ -30,6 +34,7 @@ class Range{
 
             }
         }
+        // For Decreasing order
         else{
 
             for(int i = firstValue, x = 0; x < elementQty; i-= stepSize, x++){
@@ -46,19 +51,19 @@ class Range{
 	public static void main(String args[]){
 		
         // Test Case # 1
-		int arr[] = range(1,10,1);
+		int arr[] = range(1,10);
 		System.out.println("" + Arrays.toString(arr));
 
         // Test Case # 2
-        arr = range(-1,3,1);
+        arr = range(-1,3);
 		System.out.println("" + Arrays.toString(arr));
 
         // Test Case # 3
-        arr = range(1,-3,1);
+        arr = range(1,-3);
 		System.out.println("" + Arrays.toString(arr));
 
         // Test Case # 4
-        arr = range(-1,-5,1);
+        arr = range(-1,-5);
 		System.out.println("" + Arrays.toString(arr));
 
         // Test Case # 1
